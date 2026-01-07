5213b4a3409643c244186b9dca268a901f13cb65
luusss <s1411222002@gmail.com>
2026-01-07 20:37:36 +0800

    123

--- Public/js/ffff.js
+++ Public/js/ffff.js
@@ -8,14 +8,13 @@ var luusssApp=createApp({
     }
 }).mount("#luusss");
 
-
 $.ajax({
-    url:"/luusss",
-    method:"get",
-    dataType:"json",
-    success: (result)=>{
-        luusssApp.luusss = result;
+    url: "/luusss",
+    method: "get",
+    dataType: "json",
+    success: (result) => {
+      console.log(result);     // ← 用來確認真的有資料
+      app.luusss = result;     // ← 塞進 Vue
     }
-})
-
+  });
 
