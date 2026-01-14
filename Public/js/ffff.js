77309ea34b00c3f96dec379193d5eb824ab9c774
luusss <s1411222002@gmail.com>
2026-01-14 12:29:32 +0800

    註解

--- Public/js/ffff.js
+++ Public/js/ffff.js
@@ -1,19 +1,21 @@
+// 建立一個新的 Vue 實例，並將它掛載到 id 為 'luusss' 的 HTML 元素上
 var app = new Vue({
     el: '#luusss',
     data: {
-      luusss: []
+      luusss: []   // 用來存放從後端 API 取得的資料
     },
     mounted() {
       var self = this
       $.ajax({
         url: '/luusss',   // ← 依你後端實際 API
-        method: 'GET',
+        method: 'GET',    // HTTP 方法，這裡是 GET，用來取得資料
         success: function (data) {
-          self.luusss = data
+          self.luusss = data  // 將取得的資料存入 Vue 的 data.luusss，畫面會自動更新
         }
       })
     }
   })
+
   
 
 
